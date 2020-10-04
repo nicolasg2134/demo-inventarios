d789505058cfaee1b9c841a5e5a5e303589a7bfb
nicolasg2134 <nicolasg2134@gmail.com>
2020-10-04 20:18:45 -0200

    sistema

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2020 <a href="http://www.lithink.us" target="_blank">Lithink software</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
