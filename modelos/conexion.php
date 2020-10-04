51d1f3228816177662d81535f5800b5890c55366
nicolasg2134 <nicolasg2134@gmail.com>
2020-10-04 20:46:04 -0300

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=sis_inventario",
-			            "root",
-			            "123123");
+		$link = new PDO("mysql:host=w1kr9ijlozl9l79i.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=jnzg8wzhk5fyhzxd",
+			            "yxp9ws1ljdru1oc1",
+			            "jhmaguzyqhkofugn");
 
 		$link->exec("set names utf8");
 
