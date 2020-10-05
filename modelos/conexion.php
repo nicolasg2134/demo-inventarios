90c1de43f3312fbb73a305aaceb30e79c8096ab2
nicolasg2134 <nicolasg2134@gmail.com>
2020-10-05 10:34:10 -0300

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=w1kr9ijlozl9l79i.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";"dbname=jnzg8wzhk5fyhzxd",
+		$link = new PDO("mysql:host=w1kr9ijlozl9l79i.chr7pe7iynqr.eu-west-1.rds.amazonaws.com, dbname=jnzg8wzhk5fyhzxd",
 			            "yxp9ws1ljdru1oc1",
 			            "jhmaguzyqhkofugn");
 
