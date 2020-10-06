96ffa0cd8ccbf9dc0f2934de48f1a3b58082fc21
nicolasg2134 <nicolasg2134@gmail.com>
2020-10-06 13:46:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -14,30 +14,5 @@ require_once "modelos/clientes.modelo.php";
 require_once "modelos/ventas.modelo.php";
 require_once "extensiones/vendor/autoload.php";
 
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.22.0/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.22.0/firebase-analytics.js"></script>
-
-<script>
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyC4Dwz6m4O96XVYnzuZygB9LeYSN_WTnvE",
-    authDomain: "inventory-system-5c3c3.firebaseapp.com",
-    databaseURL: "https://inventory-system-5c3c3.firebaseio.com",
-    projectId: "inventory-system-5c3c3",
-    storageBucket: "inventory-system-5c3c3.appspot.com",
-    messagingSenderId: "202783100209",
-    appId: "1:202783100209:web:a150a3198bf1db3ad93410",
-    measurementId: "G-K5QGLJLZMD"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
-
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrPlantilla();
